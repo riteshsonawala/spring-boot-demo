334a6430bcdd8c79f2c5df2b6ffb0a2cdb08ad04
ritesh <ritesh.sonawala@gmail.com>
2019-04-07 08:48:25 +0100

    Initial add

---
+++ src/main/java/com/riteshsonawala/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.riteshsonawala.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String handleRequest(){
+        return "Hello World";
+    }
+}
